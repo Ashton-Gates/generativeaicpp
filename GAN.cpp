17a0e805d363f539ddf605bcef2d607088345cd6
Ashton Kinnell <113641583+Ashton-Gates@users.noreply.github.com>
2023-09-18 11:25:31 -0400

    Update GAN.cpp

--- GAN.cpp
+++ GAN.cpp
@@ -29,14 +29,79 @@ private:
         auto layer = Add(scope, MatMul(scope, x, w), b);
         return layer; // Again, this is basic. You'd have more layers and activations
     }
+    // Placeholders for input data
+    Placeholder x;  // Real samples
+    Placeholder z;  // Noise samples
+
+    // Model outputs
+    Output real_logits;
+    Output fake_logits;
+
+    // Losses
+    Output d_loss;
+    Output g_loss;
+
+    // Optimizers
+    std::unique_ptr<tensorflow::Optimizer> d_optimizer;
+    std::unique_ptr<tensorflow::Optimizer> g_optimizer;
 
 public:
     GAN() : scope(Scope::NewRootScope()), session(scope) {
         // Initialization code here
-    }
+        // Define placeholders
+        x = Placeholder(scope, DT_FLOAT);
+        z = Placeholder(scope, DT_FLOAT);
 
+        // Define the model outputs
+        real_logits = discriminator(x);
+        fake_logits = discriminator(generator(z));
+
+        // Define the losses
+        d_loss = Mean(scope, SigmoidCrossEntropyWithLogits(scope, real_logits, Const(scope, 1.0f)), {0});
+        d_loss = Mean(scope, SigmoidCrossEntropyWithLogits(scope, fake_logits, Const(scope, 0.0f)), {0});
+
+        g_loss = Mean(scope, SigmoidCrossEntropyWithLogits(scope, fake_logits, Const(scope, 1.0f)), {0});
+
+        // Define the optimizers
+        d_optimizer = std::make_unique<tensorflow::train::GradientDescentOptimizer>(scope, 0.001f);
+        g_optimizer = std::make_unique<tensorflow::train::GradientDescentOptimizer>(scope, 0.001f);
+
+        // Add the optimization operations
+        d_optimizer->Minimize(scope, d_loss);
+        g_optimizer->Minimize(scope, g_loss);
+    }
+    void saveSamplesToDisk(const Tensor& samples, const std::string& directory) {
+    // Convert tensor to PNG format
+    auto encoded_images = EncodePng(scope, samples);
+
+    // Write each image to disk
+    for (int i = 0; i < samples.dim_size(0); ++i) {
+        // Construct the filename
+        std::string filename = io::JoinPath(directory, "sample_" + std::to_string(i) + ".png");
+
+        // Write the image to disk
+        WriteFile write_op(scope, filename, encoded_images[i]);
+        TF_CHECK_OK(session.Run({}, {}, {write_op.operation}));
     void train() {
-        // Define your training loop, loss functions, and optimizers here
+        const int num_epochs = 10000;
+        const int batch_size = 64;
+
+        for (int epoch = 0; epoch < num_epochs; ++epoch) {
+            // Fetch real samples and noise samples here
+            Tensor real_samples = ...; // Fetch real samples
+            Tensor noise_samples = ...; // Generate random noise samples
+
+            // Train discriminator
+            std::vector<Tensor> d_outputs;
+            TF_CHECK_OK(session.Run({{x, real_samples}, {z, noise_samples}}, {d_loss}, &d_outputs));
+
+            // Train generator
+            std::vector<Tensor> g_outputs;
+            TF_CHECK_OK(session.Run({{z, noise_samples}}, {g_loss}, &g_outputs));
+
+            // Print losses
+            std::cout << "Epoch: " << epoch << ", D Loss: " << d_outputs[0].scalar<float>() << ", G Loss: " << g_outputs[0].scalar<float>() << std::endl;
+        }
     }
 
     Tensor generate(const Tensor& noise) {
@@ -45,16 +110,61 @@ public:
         session.Run({generator(noise)}, &output);
         return output;
     }
+    // Define placeholders
+        x = Placeholder(scope, DT_FLOAT);
+        z = Placeholder(scope, DT_FLOAT);
+
+        // Define the model outputs
+        real_logits = discriminator(x);
+        fake_logits = discriminator(generator(z));
+
+        // Define the losses
+        d_loss = Mean(scope, SigmoidCrossEntropyWithLogits(scope, real_logits, Const(scope, 1.0f)), {0});
+        d_loss = Mean(scope, SigmoidCrossEntropyWithLogits(scope, fake_logits, Const(scope, 0.0f)), {0});
+
+        g_loss = Mean(scope, SigmoidCrossEntropyWithLogits(scope, fake_logits, Const(scope, 1.0f)), {0});
+
+        // Define the optimizers
+        d_optimizer = std::make_unique<tensorflow::train::GradientDescentOptimizer>(scope, 0.001f);
+        g_optimizer = std::make_unique<tensorflow::train::GradientDescentOptimizer>(scope, 0.001f);
+
+        // Add the optimization operations
+        d_optimizer->Minimize(scope, d_loss);
+        g_optimizer->Minimize(scope, g_loss);
+    }
 };
 
 int main() {
     GAN gan;
     // Train the GAN
     gan.train();
-
+    
     // Generate samples
     Tensor noise = ...; // Create a noise tensor
     Tensor samples = gan.generate(noise);
+    
+      const int num_epochs = 10000;
+        const int batch_size = 64;
 
-    // Do something with the samples, e.g., save them to disk
+        for (int epoch = 0; epoch < num_epochs; ++epoch) {
+            // Fetch real samples and noise samples here
+            // For simplicity, we're using random values
+            Tensor real_samples = ...; // Fetch real samples
+            Tensor noise_samples = ...; // Generate random noise samples
+
+            // Train discriminator
+            std::vector<Tensor> d_outputs;
+            TF_CHECK_OK(session.Run({{x, real_samples}, {z, noise_samples}}, {d_loss}, &d_outputs));
+
+            // Train generator
+            std::vector<Tensor> g_outputs;
+            TF_CHECK_OK(session.Run({{z, noise_samples}}, {g_loss}, &g_outputs));
+
+            // Print losses
+            std::cout << "Epoch: " << epoch << ", D Loss: " << d_outputs[0].scalar<float>() << ", G Loss: " << g_outputs[0].scalar<float>() << std::endl;
+
+            // Save the samples to disk
+            saveSamplesToDisk(samples, "./generated_samples");
+        }
+    }
 }
